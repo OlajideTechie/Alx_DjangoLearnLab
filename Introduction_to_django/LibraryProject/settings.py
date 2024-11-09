4992957bc2fe468634677199adccb76f22497e5d
Olajide Ojo <oolajide91@gmail.com>
2024-11-09 16:40:18 +0100

    django folder restructure2

--- Introduction_to_django/LibraryProject/settings.py
+++ Introduction_to_django/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rfac5h3(km4z^tfua0nlzw6$o*g!7td(=a2srdah#^nfioqi@_"
+SECRET_KEY = "django-insecure-c8mpcbmbn-l$jw4w_zno)emu81eks@&6qgnhfu2&0!tek3=^3)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bookshelf"
+    "bookshelf",
 ]
 
 MIDDLEWARE = [
