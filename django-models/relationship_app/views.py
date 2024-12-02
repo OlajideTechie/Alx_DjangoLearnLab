14fee483da4687f845227ed65c02cfa6248e49fc
Olajide Ojo <oolajide91@gmail.com>
2024-12-02 20:36:00 +0100

    import library

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,8 @@
 # relationship_app/views.py
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Function-based view to display a list of books and their authors
 def book_list(request):
