90a061d2cda8a97f09251cebd449e61c69419586
Olajide Ojo <oolajide91@gmail.com>
2024-12-02 20:45:45 +0100

    import list_books class

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Book
 from .models import Library
 
 # Function-based view to display a list of books and their authors
-def book_list(request):
+def list_books(request):
     """Retrieves all books and renders a template displaying the list."""
     books = Book.objects.all()  # Fetch all book instances from the database
     return render(request, 'relationship_app/list_books.html', {'books': books})
